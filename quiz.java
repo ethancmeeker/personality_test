841d567643ae3acfcb51eb2eca2f11ae471fb210
ethancmeeker <91624529+ethancmeeker@users.noreply.github.com>
2022-01-01 15:01:07 -0700

    Create quiz.java

---
+++ quiz.java
@@ -0,0 +1,212 @@
+import javax.swing.JOptionPane;
+import java.util.Random;
+import java.util.*; 
+import java.lang.*; 
+public class findJob2 {
+   public static String[] thanos = new String[]{"1", "2", "3", "4"};
+   public static void main(String[] args) {
+      String nema = null;
+      String toto = null;
+      String scratch = null;
+      String haunt = null;
+      String lost = null;
+      String cake = null;
+      String wario = null;
+      String yeet = null;
+      String garden = null;
+      String cuch = null;
+      String wha = null;
+      String momo = null;
+      String boi = null;
+      String tide = null;
+      String holy = null;
+      String lele = null;
+      String gaga = null;
+      String swag = null;
+      String star = null;
+      String yam = null;
+      String tepe = null;
+      String queen = null;
+      String jacob = null;
+      String pea = null;
+      String pee = null;
+      String yuk = null;
+      String bear = null;
+      String lala = null;
+      String curb = null;
+      String gold = null;
+      String turbo = null;      
+      int sweet = 0;
+      Random rand = new Random();
+      JOptionPane.showMessageDialog(null, "Time to find out what future job you will have", "why hello there", JOptionPane.PLAIN_MESSAGE );
+      nema = JOptionPane.showInputDialog(null, "What is your name of you're true self (If you believe you were born wrong, enter in your real name)", "WALUIGI", JOptionPane.PLAIN_MESSAGE );
+      JOptionPane.showMessageDialog(null, "Alright, let's get cracking", nema, JOptionPane.PLAIN_MESSAGE );
+      toto = (String) JOptionPane.showInputDialog(null, "What are your goals in life?\n1.To eat an entire chocolate cake\n2.Become a clown\n3.Dedicate your life to shrek\n4.Steal the crown jewels?", nema, JOptionPane.QUESTION_MESSAGE, null, thanos, thanos);
+      scratch = (String) JOptionPane.showInputDialog(null, "What kind of milk do you prefer?\n1.Cow's milk\n2.Goat's milk\n3.Soy Milk\n4.Pidgeon Milk", nema, JOptionPane.QUESTION_MESSAGE, null, thanos, thanos);
+      haunt = (String) JOptionPane.showInputDialog(null, "What word describes you best?\n1.Gucci\n2.Cowish\n3.White\n4.Bigot", nema, JOptionPane.QUESTION_MESSAGE, null, thanos, thanos);
+      lost = (String) JOptionPane.showInputDialog(null, "What is your favorite movie?\n1.The Little Mermaid 2\n2.MegaBlox Movie\n3.The Emoji Movie\n4.The killer bee movie", nema, JOptionPane.QUESTION_MESSAGE, null, thanos, thanos);
+      cake = (String) JOptionPane.showInputDialog(null, "What is your favorite pass time?\n1.Kidnapping\n2.Toilet Cleaning\n3.Watching Nick Jr.\n4.Entering through the exit door at walmart", nema, JOptionPane.QUESTION_MESSAGE, null, thanos, thanos);
+      wario = (String) JOptionPane.showInputDialog(null, "What is your favorite subject at school?\n1.Biting\n2.Meth Lab\n3.Cartoon Physics\n4.Fidget Spinner Class", nema, JOptionPane.QUESTION_MESSAGE, null, thanos, thanos);
+      yeet = (String) JOptionPane.showInputDialog(null, "Who is your ideal spouse?\n1.Looks and acts like Waluigi\n2.Two teeth\n3.Made of LEGOS\n4.Beutiful but with a terrible personality", nema, JOptionPane.QUESTION_MESSAGE, null, thanos, thanos);
+      garden = (String) JOptionPane.showInputDialog(null, "What is the best place to eat?\n1.McDonalds\n2.The Dumpster\n3.An elementary school\n4.Petsmart", nema, JOptionPane.QUESTION_MESSAGE, null, thanos, thanos);
+      cuch = (String) JOptionPane.showInputDialog(null, "What is your favorite video game?\n1.Waluigi's Taco Stand\n2.Shrek Super Slam\n3.Playstation Allstars\n4.Pong 2", nema, JOptionPane.QUESTION_MESSAGE, null, thanos, thanos);
+      wha = (String) JOptionPane.showInputDialog(null, "What is the best quality product?\n1.Totino's Pizza\n2.Huggies Diapers\n3.LEGO starwars\n4.Trident Gum", nema, JOptionPane.QUESTION_MESSAGE, null, thanos, thanos); 
+      momo = (String) JOptionPane.showInputDialog(null, "What duck is best?\n1.Mallard\n2.Canvasback\n3.DuckHunt ducks\n4.Mallac", nema, JOptionPane.QUESTION_MESSAGE, null, thanos, thanos);
+      boi = (String) JOptionPane.showInputDialog(null, "What programming launguage do you like the most?\n1.Java\n2.Python\n3.Scratch\n4.HTML", nema, JOptionPane.QUESTION_MESSAGE, null, thanos, thanos);
+      tide = (String) JOptionPane.showInputDialog(null, "What is the best name for a baby girl?\n1.Uvula\n2.Bethsheba\n3.Helgagatha\n4.Abcde", nema, JOptionPane.QUESTION_MESSAGE, null, thanos, thanos);
+      holy = (String) JOptionPane.showInputDialog(null, "What is the best gaming consule?\n1.Soulja Boy\n2.vSmile\n3.Playstation Swap\n4.Virtual Boy", nema, JOptionPane.QUESTION_MESSAGE, null, thanos, thanos);
+      lele = (String) JOptionPane.showInputDialog(null, "What is your favorite color?\n1.Vomit Green\n2.Mac & Cheese\n3.Blackish White\n4.Blood Red", nema, JOptionPane.QUESTION_MESSAGE, null, thanos, thanos);
+      swag = (String) JOptionPane.showInputDialog(null, "What is your favorite scent?\n1.Tuesday\n2.Circle K Bathroom\n3.George Washington\n4.Shrek's Swamp", nema, JOptionPane.QUESTION_MESSAGE, null, thanos, thanos);
+      gaga = (String) JOptionPane.showInputDialog(null, "How do you want to die?\n1.Burn in a swimming pool\n2.Get a McDonald's toy lodged in your throat\n3.Be crushed by an anvil\n4.A dare", nema, JOptionPane.QUESTION_MESSAGE, null, thanos, thanos);
+      tepe = (String) JOptionPane.showInputDialog(null, "Who is your celebrity girl crush?\n1.Kim Kardashian\n2.Hillary Clinton\n3.Elsa\n4.Scarlet Johansen", nema, JOptionPane.QUESTION_MESSAGE, null, thanos, thanos);
+      star = (String) JOptionPane.showInputDialog(null, "Who is your celebrity man crush?\n1.Lebron James\n2.Freddy Mercury\n3.James Charles\n4.DanTDM", nema, JOptionPane.QUESTION_MESSAGE, null, thanos, thanos);
+      yam = (String) JOptionPane.showInputDialog(null, "Who is the best Chris?\n1.Chris Pratt\n2.Chris Pine\n3.Chris Hemsworth\n4.Chris Evans", nema, JOptionPane.QUESTION_MESSAGE, null, thanos, thanos);
+      queen = (String) JOptionPane.showInputDialog(null, "What is youor favorite dessert?\n1.100% cacao chocolate\n2.Cucumber Cake\n3.Pillow\n4.Windex Cookies", nema, JOptionPane.QUESTION_MESSAGE, null, thanos, thanos);
+      curb = (String) JOptionPane.showInputDialog(null, "What is the best number?\n1.3\n2.1\n3.4\n4.2", nema, JOptionPane.QUESTION_MESSAGE, null, thanos, thanos);
+      yuk = (String) JOptionPane.showInputDialog(null, "Who do you worship?\n1.Mr Bear\n2.Shrek\n3.Chuck Norris\n4.LEGO Yoda", nema, JOptionPane.QUESTION_MESSAGE, null, thanos, thanos);
+      pea = (String) JOptionPane.showInputDialog(null, "Who should get into smash bros?\n1.Big the Cat\n2.Psyduck\n3.Charles Darwin\n4.Waluigi", nema, JOptionPane.QUESTION_MESSAGE, null, thanos, thanos);
+      pee = (String) JOptionPane.showInputDialog(null, "What is your weapon of choice?\n1.Flip Flop\n2.James Charles\n3.Bra\n4.Butterknife", nema, JOptionPane.QUESTION_MESSAGE, null, thanos, thanos);
+      lala = (String) JOptionPane.showInputDialog(null, "What is the best kind of water?\n1.Aquafina\n2.Polluted Lake\n3.Dasani\n4.Member's Mark", nema, JOptionPane.QUESTION_MESSAGE, null, thanos, thanos);
+      gold = (String) JOptionPane.showInputDialog(null, "What word is the sexiest?\n1.Whaa\n2.Corn\n3.Demon\n4.Oreo", nema, JOptionPane.QUESTION_MESSAGE, null, thanos, thanos);
+      turbo = (String) JOptionPane.showInputDialog(null, "Where is the best place to hang out?\n1.Toys-r-us\n2.Home Depot Bathroom\n3.Creepy Ally Ways\n4.Christian Minecraft Server", nema, JOptionPane.QUESTION_MESSAGE, null, thanos, thanos);
+      bear = (String) JOptionPane.showInputDialog(null, "What is your choice of pants?\n1.Oversized Jeans\n2.Speedo\n3.Barrel\n4.Short Shorts", nema, JOptionPane.QUESTION_MESSAGE, null, thanos, thanos);
+      jacob = (String) JOptionPane.showInputDialog(null, "What kind of house do you want?\n1.Haunted House\n2.Shack on the Beach\n3.Van in a Walmart Parking Lot\n4.Cardboard Box", nema, JOptionPane.QUESTION_MESSAGE, null, thanos, thanos);
+      
+            JOptionPane.showMessageDialog(null, "CALCULATING", nema, JOptionPane.PLAIN_MESSAGE );
+      int totoq = Integer.parseInt(toto);
+      int scratchq = Integer.parseInt(scratch);
+      int hauntq = Integer.parseInt(haunt);
+      int lostq = Integer.parseInt(lost);
+      int cakeq = Integer.parseInt(cake);
+      int warioq = Integer.parseInt(wario);
+      int yeetq = Integer.parseInt(yeet);
+      int gardenq = Integer.parseInt(garden);
+      int cuchq = Integer.parseInt(cuch);
+      int whaq = Integer.parseInt(wha);
+      int momoq = Integer.parseInt(momo);
+      int tideq = Integer.parseInt(tide);
+      int boiq = Integer.parseInt(boi);
+      int gagaq = Integer.parseInt(gaga);
+      int leleq = Integer.parseInt(lele);
+      int holyq = Integer.parseInt(holy);
+      int swagq = Integer.parseInt(swag);
+      int yamq = Integer.parseInt(yam);
+      int starq = Integer.parseInt(star);
+      int tepeq = Integer.parseInt(tepe);
+      int queenq = Integer.parseInt(queen);
+      int turboq = Integer.parseInt(turbo);
+      int yukq = Integer.parseInt(yuk);
+      int jacobq = Integer.parseInt(jacob);
+      int lalaq = Integer.parseInt(lala);
+      int goldq = Integer.parseInt(gold);
+      int peaq = Integer.parseInt(pea);
+      int peeq = Integer.parseInt(pee);
+      int curbq = Integer.parseInt(curb);
+      int bearq = Integer.parseInt(bear);      
+      sweet = (totoq + scratchq + hauntq + lostq + cakeq + warioq + yeetq + gardenq + whaq + cuchq + momoq + boiq + tepeq + yamq + starq + tideq + leleq + holyq + swagq + gagaq + bearq + turboq + jacobq + lalaq + queenq + peaq + peeq + curbq + yukq + goldq);
+      if (40 < sweet){
+         if (44 > sweet) {
+            JOptionPane.showMessageDialog(null, "You would be a great KISS UP", nema, JOptionPane.PLAIN_MESSAGE ); }
+         }
+      if (68 < sweet){
+         if (72 > sweet){
+            JOptionPane.showMessageDialog(null, "You would be a great SPIDER-MAN", nema, JOptionPane.PLAIN_MESSAGE ); }
+            }
+      if (71 < sweet){
+         if (75 > sweet){
+            JOptionPane.showMessageDialog(null, "You would be a great JANITOR'S SECRETARY", nema, JOptionPane.PLAIN_MESSAGE );}}
+      if (74 < sweet){
+         if (78 > sweet){
+           JOptionPane.showMessageDialog(null, "You would be a great IRON MAN", nema, JOptionPane.PLAIN_MESSAGE );}}
+      if (83 < sweet){
+         if (87 > sweet){
+            JOptionPane.showMessageDialog(null, "You would be a great OUTSIDE FOOD OR DRINK IN THE MOVIE THEATER", nema, JOptionPane.PLAIN_MESSAGE );}}
+      if (86 < sweet){
+         if (90 > sweet){
+            JOptionPane.showMessageDialog(null, "You would be a great LAWYER", nema, JOptionPane.PLAIN_MESSAGE );}}
+      if (89 < sweet){
+         if (94 > sweet){
+            JOptionPane.showMessageDialog(null, "You would be a great ETHAN MEEKER'S SLAVE", nema, JOptionPane.PLAIN_MESSAGE );}}
+      if (43 < sweet){
+         if (47 > sweet){
+            JOptionPane.showMessageDialog(null, "You would be a great TAYLOR SWIFT", nema, JOptionPane.PLAIN_MESSAGE );}}
+      if (29 < sweet){
+         if (33 > sweet){
+            JOptionPane.showMessageDialog(null, "You would be a great FORTNITE YOUTUBER WHO SWEARS", nema, JOptionPane.PLAIN_MESSAGE );}}
+      if (27 < sweet){
+         if (30 > sweet){
+            JOptionPane.showMessageDialog(null, "You would be a great INTERNET DOUCHE BAG", nema, JOptionPane.PLAIN_MESSAGE );}}
+      if (80 < sweet){
+         if (84 > sweet){
+           JOptionPane.showMessageDialog(null, "You would be a great SEWAGE WORKER", nema, JOptionPane.PLAIN_MESSAGE );}}
+      if (32 < sweet){
+         if (36 > sweet){
+            JOptionPane.showMessageDialog(null, "You would be a great DISSAPOINTMENT", nema, JOptionPane.PLAIN_MESSAGE );}}
+      if (38 < sweet){  
+         if (41 > sweet){
+            JOptionPane.showMessageDialog(null, "You would be a great AMATUER LEGO STOPMOTION ARTIST", nema, JOptionPane.PLAIN_MESSAGE );}}     
+      if (30 > sweet){
+         JOptionPane.showMessageDialog(null, "You would make a great PERSON WHO HATES TAKING JOB  QUIZES SO PRESSES THE X BUTTON EVERYTIME", nema, JOptionPane.PLAIN_MESSAGE );}
+      if (35 < sweet){  
+         if (39 > sweet){
+            JOptionPane.showMessageDialog(null, "You would be a great POKEMON MASTER", nema, JOptionPane.PLAIN_MESSAGE );}}     
+      if (46 < sweet){  
+         if (49 > sweet){
+            JOptionPane.showMessageDialog(null, "You would be a great MEME", nema, JOptionPane.PLAIN_MESSAGE );}}     
+      if (48 < sweet){  
+         if (53 > sweet){
+            JOptionPane.showMessageDialog(null, "You would be a great MAIN METHOD", nema, JOptionPane.PLAIN_MESSAGE );}}
+      if (52 < sweet){  
+         if (56 > sweet){
+            JOptionPane.showMessageDialog(null, "You would be a great LOVLY LADY", nema, JOptionPane.PLAIN_MESSAGE );}}                     
+      if (55 < sweet){  
+         if (58 > sweet){
+            JOptionPane.showMessageDialog(null, "You would be a great HONEY BOO BOO", nema, JOptionPane.PLAIN_MESSAGE );}}          
+      if (57 < sweet){  
+         if (60 > sweet){
+            JOptionPane.showMessageDialog(null, "You would be a great HUNTCHBACK", nema, JOptionPane.PLAIN_MESSAGE );}}
+      if (59 < sweet){  
+         if (64 > sweet){
+            JOptionPane.showMessageDialog(null, "You would be a great MR BEAR", nema, JOptionPane.PLAIN_MESSAGE );}}
+      if (63 < sweet){  
+         if (66 > sweet){
+            JOptionPane.showMessageDialog(null, "You would be a great ORGAN DONOR", nema, JOptionPane.PLAIN_MESSAGE );}} 
+      if (65 < sweet){  
+         if (69 > sweet){
+            JOptionPane.showMessageDialog(null, "You would be a great SWEATY PAUL", nema, JOptionPane.PLAIN_MESSAGE );}}                                             
+      if (77 < sweet){  
+         if (81 > sweet){
+            JOptionPane.showMessageDialog(null, "You would be a great INFANT", nema, JOptionPane.PLAIN_MESSAGE );}}
+      if (93 < sweet){  
+         if (97 > sweet){
+            JOptionPane.showMessageDialog(null, "You would be a great TIKTHOT", nema, JOptionPane.PLAIN_MESSAGE );}}                                                          
+      
+      if (96 < sweet){  
+         if (101 > sweet){
+            JOptionPane.showMessageDialog(null, "You would be a great REDNECK", nema, JOptionPane.PLAIN_MESSAGE );}}                                                          
+      
+      if (100 < sweet){  
+         if (105 > sweet){
+            JOptionPane.showMessageDialog(null, "You would be a great ROBLOX GIRLFRIEND", nema, JOptionPane.PLAIN_MESSAGE );}}                                                          
+      if (104 < sweet){  
+         if (109 > sweet){
+            JOptionPane.showMessageDialog(null, "You would be a great TOILET SCRUBBER", nema, JOptionPane.PLAIN_MESSAGE );}}      
+      if (108 < sweet){  
+         if (113 > sweet){
+            JOptionPane.showMessageDialog(null, "You would be a great HOTMAIL PROGRAMMER", nema, JOptionPane.PLAIN_MESSAGE );}}
+      if (112 < sweet){  
+         if (117 > sweet){
+            JOptionPane.showMessageDialog(null, "You would be a great WALUIGI PROPHET", nema, JOptionPane.PLAIN_MESSAGE );}}   
+      if (116 < sweet){  
+         if (121 > sweet){
+            JOptionPane.showMessageDialog(null, "You would be a great TRANSGENDER", nema, JOptionPane.PLAIN_MESSAGE );}}                                                                                                                                                                                                               
+      }
+   }
+   
+             
+      
+   
+   
+   
+
+
